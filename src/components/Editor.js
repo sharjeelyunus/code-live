fd5ca02c9bdd9575fd74f5238f068993367909b7
sharjeelyunus <sharjeel924@gmail.com>
2021-11-26 21:47:50 +0500

    📦 NEW: Edittor Setup

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from 'react';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/dracula.css';
+import 'codemirror/mode/xml/xml';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/mode/css/css';
+import { Controlled as ControlledEditor } from 'react-codemirror2';
 
-const Editor = () => {
+
+const Editor = (props) => {
+    const {
+        language,
+        displayName,
+        value,
+        onChange
+    } = props;
+
+    function handleChange(editor, data, value) {
+        onChange(value)
+    }
+
     return (
-        <div>
-            Editor
+        <div className="editor-container">
+            <div className="editor-title">
+                {displayName}
+                <button>O/C</button>
+            </div>
+            <ControlledEditor
+                onBeforeChange={handleChange}
+                value={value}
+                className="code-mirror-wrapper"
+                options={{
+                    lineWrapping: true,
+                    link: true,
+                    mode: language,
+                    lineNumbers: true,
+                    theme: 'dracula'
+                }}
+            />
         </div>
     )
 }
